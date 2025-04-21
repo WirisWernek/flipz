b1d7f4899777ed85d1d455ee0f6ac1d6fd7c7520
Wiris Wernek <wiriswernek@gmail.com>
2025-04-21 02:56:30 -0300

    feat: cria base do componente list-colecao

---
+++ src/app/pages/views/colecao/list-colecao/list-colecao.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-colecao',
+  imports: [],
+  templateUrl: './list-colecao.component.html',
+  styleUrl: './list-colecao.component.scss'
+})
+export class ListColecaoComponent {
+
+}
