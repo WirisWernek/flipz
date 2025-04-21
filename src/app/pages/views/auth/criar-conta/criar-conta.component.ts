3d7034d1444a1f7c4b469c188c6b2b78707f666e
Wiris Wernek <wiriswernek@gmail.com>
2025-04-21 02:55:05 -0300

    feat: cria base do componente de criar conta

---
+++ src/app/pages/views/auth/criar-conta/criar-conta.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-conta',
+  imports: [],
+  templateUrl: './criar-conta.component.html',
+  styleUrl: './criar-conta.component.scss'
+})
+export class CriarContaComponent {
+
+}
