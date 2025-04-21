1a9d6938831e5d7b25a82926da7063cfd31b8c43
Wiris Wernek <wiriswernek@gmail.com>
2025-04-21 03:20:15 -0300

    feat: cria base do modal-form-deck

---
+++ src/app/pages/views/deck/modal-form-deck/modal-form-deck.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-form-deck',
+  imports: [],
+  templateUrl: './modal-form-deck.component.html',
+  styleUrl: './modal-form-deck.component.scss'
+})
+export class ModalFormDeckComponent {
+
+}
