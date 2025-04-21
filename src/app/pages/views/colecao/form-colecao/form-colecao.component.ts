47ce5a610bf1a583a139c55e030ee25b2c3178ed
Wiris Wernek <wiriswernek@gmail.com>
2025-04-21 02:56:13 -0300

    feat: cria base do componente form-colecao

---
+++ src/app/pages/views/colecao/form-colecao/form-colecao.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-colecao',
+  imports: [],
+  templateUrl: './form-colecao.component.html',
+  styleUrl: './form-colecao.component.scss'
+})
+export class FormColecaoComponent {
+
+}
