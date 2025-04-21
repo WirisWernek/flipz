611436e08ea159dbcf39d0adddee49870e81a02b
Wiris Wernek <wiriswernek@gmail.com>
2025-04-21 17:14:32 -0300

    feat: implementa formulário de login com validação de email e senha

--- src/app/pages/views/auth/login/login.component.ts
+++ src/app/pages/views/auth/login/login.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms'
+import { MaterialModule } from '../../../../shared/modules/material/material.module'
 
 @Component({
-  selector: 'app-login',
-  imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrl: './login.component.scss',
+    imports: [MaterialModule, FormsModule, ReactiveFormsModule],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginComponent {
+    senha = true
 
+    form: FormGroup = new FormGroup({
+        email: new FormControl<string>('', [Validators.required, Validators.email]),
+        password: new FormControl<string>('', [Validators.required, Validators.minLength(6)]),
+    })
+
+    avancar() {
+        if (this.form.controls['email'].valid) {
+            this.senha = false
+        }
+    }
+
+    login() {}
 }
