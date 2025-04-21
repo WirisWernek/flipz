a7045308789174100c8930e85d52e03f9235ca1a
Wiris Wernek <wiriswernek@gmail.com>
2025-04-21 03:20:39 -0300

    feat: cria base do componente list-deck

---
+++ src/app/pages/views/deck/list-deck/list-deck.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-deck',
+  imports: [],
+  templateUrl: './list-deck.component.html',
+  styleUrl: './list-deck.component.scss'
+})
+export class ListDeckComponent {
+
+}
