d7f0d60af59d2369098d3890aa9c88820f72a58e
Wiris Wernek <wiriswernek@gmail.com>
2025-04-21 03:01:01 -0300

    feat: adiciona rotas para as páginas de autenticação, coleções, grupos e flash cards

---
+++ src/app/pages/pages.routes.ts
@@ -0,0 +1,29 @@
+import { Routes } from "@angular/router";
+
+export const PAGES_ROUTES: Routes = [
+	{
+		path: 'auth',
+		loadComponent: () => import('./layouts/auth/auth.component').then(c => c.AuthComponent),
+		loadChildren: () => import('./views/auth/auth.routes').then(r => r.AUTH_ROUTES)
+	},
+	{
+		path: 'colecao',
+		loadComponent: () => import('./layouts/default/default.component').then(c => c.DefaultComponent),
+		loadChildren: () => import('./views/colecao/colecao.routes').then(r => r.COLECAO_ROUTES)
+	},
+	{
+		path: 'grupo',
+		loadComponent: () => import('./layouts/default/default.component').then(c => c.DefaultComponent),
+		loadChildren: () => import('./views/grupo/grupo.routes').then(r => r.GRUPO_ROUTES)
+	},
+	{
+		path: 'flash-card',
+		loadComponent: () => import('./layouts/default/default.component').then(c => c.DefaultComponent),
+		loadChildren: () => import('./views/flash-card/flash-card.routes').then(r => r.FLASH_CARD_ROUTES)
+	},
+	{
+		path: '**',
+		pathMatch: 'full',
+		redirectTo: 'auth'
+	}
+]
