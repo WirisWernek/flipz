ad070c5e472d00f42d46bd3725285cab9d8a2f11
Wiris Wernek <wiriswernek@gmail.com>
2025-04-21 02:58:22 -0300

    feat: cria base do componente form-grupo

---
+++ src/app/pages/views/grupo/form-grupo/form-grupo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-grupo',
+  imports: [],
+  templateUrl: './form-grupo.component.html',
+  styleUrl: './form-grupo.component.scss'
+})
+export class FormGrupoComponent {
+
+}
