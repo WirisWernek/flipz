2ad2d1f08647041530f1353623319e0b3ac22624
Wiris Wernek <wiriswernek@gmail.com>
2025-04-21 02:57:28 -0300

    feat: cria base do componente list-flash-card

---
+++ src/app/pages/views/flash-card/list-flash-card/list-flash-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-flash-card',
+  imports: [],
+  templateUrl: './list-flash-card.component.html',
+  styleUrl: './list-flash-card.component.scss'
+})
+export class ListFlashCardComponent {
+
+}
