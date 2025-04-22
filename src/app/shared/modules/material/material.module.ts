2a97fdc3d090c2cf1ec826fe8a783d810869457a
Wiris Wernek <wiriswernek@gmail.com>
2025-04-21 23:43:39 -0300

    feat: adiciona MatCardModule ao módulo Material

--- src/app/shared/modules/material/material.module.ts
+++ src/app/shared/modules/material/material.module.ts
@@ -1,14 +1,14 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { MatButtonModule } from '@angular/material/button'
+import { MatCardModule } from '@angular/material/card'
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatIconModule } from '@angular/material/icon'
 import { MatInputModule } from '@angular/material/input'
 
 @NgModule({
     declarations: [],
-	imports: [CommonModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule],
-	exports: [CommonModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule],
-	
+    imports: [CommonModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, MatCardModule],
+    exports: [CommonModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, MatCardModule],
 })
 export class MaterialModule {}
