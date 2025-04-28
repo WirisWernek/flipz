cda78edd589b7145abe3927a5edd14f846d7a31b
Wiris Wernek <wiriswernek@gmail.com>
2025-04-28 02:17:55 -0300

    feat: ajusta componente de listagem de flashcards com integração ao API e estilização

--- src/app/pages/views/flash-card/list-flash-card/list-flash-card.component.ts
+++ src/app/pages/views/flash-card/list-flash-card/list-flash-card.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core'
+import { Observable } from 'rxjs'
+import { FlashCardApi } from '../../../../core/api/flash-card.api'
+import { FlashCardModel } from '../../../../core/models/flash-card.model'
+import { MaterialModule } from '../../../../shared/modules/material/material.module'
+import { SharedModule } from '../../../../shared/modules/shared/shared.module'
 
 @Component({
-  selector: 'app-list-flash-card',
-  imports: [],
-  templateUrl: './list-flash-card.component.html',
-  styleUrl: './list-flash-card.component.scss'
+    selector: 'app-list-flash-card',
+    imports: [MaterialModule, SharedModule],
+    templateUrl: './list-flash-card.component.html',
+    styleUrl: './list-flash-card.component.scss',
 })
-export class ListFlashCardComponent {
+export class ListFlashCardComponent implements OnInit {
+    private api = inject(FlashCardApi)
+    flashCards$!: Observable<FlashCardModel[]>
 
+    ngOnInit() {
+        this.flashCards$ = this.api.get()
+    }
 }
