be2ca7fc54ca708b4f07698df1a839bdeb446ebc
Wiris Wernek <wiriswernek@gmail.com>
2025-04-28 01:04:42 -0300

    feat: ajusta componente de listagem de coleções com ícones e estilos

--- src/app/pages/views/colecao/list-colecao/list-colecao.component.ts
+++ src/app/pages/views/colecao/list-colecao/list-colecao.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core'
+import { Observable } from 'rxjs'
+import { ColecaoApi } from '../../../../core/api/colecao.api'
+import { ColecaoModel } from '../../../../core/models/colecao.model'
+import { MaterialModule } from '../../../../shared/modules/material/material.module'
+import { SharedModule } from '../../../../shared/modules/shared/shared.module'
 
 @Component({
-  selector: 'app-list-colecao',
-  imports: [],
-  templateUrl: './list-colecao.component.html',
-  styleUrl: './list-colecao.component.scss'
+    selector: 'app-list-colecao',
+    imports: [MaterialModule, SharedModule],
+    templateUrl: './list-colecao.component.html',
+    styleUrl: './list-colecao.component.scss',
 })
-export class ListColecaoComponent {
+export class ListColecaoComponent implements OnInit {
+    private api = inject(ColecaoApi)
+    colecoes$!: Observable<ColecaoModel[]>
 
+    ngOnInit() {
+        this.colecoes$ = this.api.get()
+    }
 }
