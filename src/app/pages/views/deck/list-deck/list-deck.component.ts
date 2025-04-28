8226ca01dea0caebe0cd5247f984134b6ab07880
Wiris Wernek <wiriswernek@gmail.com>
2025-04-28 01:58:01 -0300

    feat: ajusta componente de listagem de decks com integração ao API e estilização

--- src/app/pages/views/deck/list-deck/list-deck.component.ts
+++ src/app/pages/views/deck/list-deck/list-deck.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core'
+import { Observable } from 'rxjs'
+import { DeckApi } from '../../../../core/api/deck.api'
+import { DeckModel } from '../../../../core/models/deck.model'
+import { MaterialModule } from '../../../../shared/modules/material/material.module'
+import { SharedModule } from '../../../../shared/modules/shared/shared.module'
 
 @Component({
-  selector: 'app-list-deck',
-  imports: [],
-  templateUrl: './list-deck.component.html',
-  styleUrl: './list-deck.component.scss'
+    selector: 'app-list-deck',
+    imports: [MaterialModule, SharedModule],
+    templateUrl: './list-deck.component.html',
+    styleUrl: './list-deck.component.scss',
 })
-export class ListDeckComponent {
+export class ListDeckComponent implements OnInit {
+    private api = inject(DeckApi)
+    decks$!: Observable<DeckModel[]>
 
+    ngOnInit() {
+        this.decks$ = this.api.get()
+    }
 }
