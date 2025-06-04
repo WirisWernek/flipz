71a856efe53fdc8c6ebac1ec73330858c3b218d0
Wiris Wernek <wiriswernek@gmail.com>
2025-06-03 22:51:25 -0300

    feat: cria componente de formulário de coleção com estilização e integração de módulos

--- src/app/pages/views/colecao/form-colecao/form-colecao.component.ts
+++ src/app/pages/views/colecao/form-colecao/form-colecao.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { MaterialModule } from '@shared/modules/material/material.module'
+import { SharedModule } from '@shared/modules/shared/shared.module'
 
 @Component({
-  selector: 'app-form-colecao',
-  imports: [],
-  templateUrl: './form-colecao.component.html',
-  styleUrl: './form-colecao.component.scss'
+    selector: 'app-form-colecao',
+    imports: [MaterialModule, SharedModule],
+    templateUrl: './form-colecao.component.html',
+    styleUrl: './form-colecao.component.scss',
 })
-export class FormColecaoComponent {
-
-}
+export class FormColecaoComponent {}
